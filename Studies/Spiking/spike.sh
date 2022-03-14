884b7d55516857aeb2d675fac12dbe389cfdf69f
rjzupkoii <rjzii@yahoo.com>
2022-03-14 16:26:11 -0400

    Parameter search spiking

---
+++ Studies/Spiking/spike.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script to run various 561H spiking combinations.
+#
+# Note that the calibrationLib.sh from PSU-CIDD-MaSim-Supoort is needed
+source ./calibrationLib.sh
+
+# Settings for the cluster
+user='rbz5100'
+replicates=0
+
+# Iterate over all of the key variables and create the replicates
+for year in `seq 2003 1 2014`; do
+  for fraction in `seq 0.005 0.005 0.1`; do
+    
+    # Prepare the files
+    configuration="rwa-spike-$year-$fraction.yml"
+    sed 's/#YEAR#/'"$year"'/g' rwa-spike-template.yml > $configuration
+    sed -i 's/#FRACTION#/'"$fraction"'/g' $configuration
+    job="rwa-spike-$year-$fraction.job"
+    sed 's/#FILENAME#/'"$configuration"'/g' rwa-spike.job > $job
+
+    # Queue the job
+    check_delay $user
+    qsub $job
+    let "replicates+=1"
+  done
+done
